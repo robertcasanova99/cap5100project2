ade26f1864bf39484b2e2672792121ba4a0f45cb
robertcasanova99 <44031505+robertcasanova99@users.noreply.github.com>
2023-04-03 23:21:12 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import { Header } from "./components/header";
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
+import "./App.css";
+
+export const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 1000,
+  speedAsDuration: true,
+});
+
+const App = () => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
+  return (
+    <div>
+      <Header data={landingPageData.Header} />
+    </div>
+  );
+};
+
+export default App;
